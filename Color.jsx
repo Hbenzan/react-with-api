69ab127db7cac697abe4448018d6eb15cbb5d191
hayesbenzan <hayes_benzan25@milton.edu>
2025-04-22 08:31:14 -0400

    finished

--- Color.jsx
+++ Color.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Colors=()=>{
-    return(
-       <>
-       <h1> This is Colors component</h1>
-       </> 
-    )
-}
+const Colors = () => {
+  return (
+    <div style={{ backgroundColor: "#f0f8ff", padding: "20px" }}> 
+      <h1>This is Colors component</h1>
+    </div>
+    //changing the background color behind colors component. 
+  );
+};
+
 export default Colors;
--- src/ Home.jsx
+++ src/ Home.jsx
@@ -1,5 +1,6 @@
 import React, { use, useEffect, useState } from "react";
-import Colors from "../Color";
+import Colors from "../Color"; // import Colors from './Colors';
+
 import axios from "axios";
 
 
@@ -9,20 +10,25 @@ const Home=()=>{
         Description: ''
     })
     const [colorsData, setColorsData] = useState([])
+        //Getting color data from the website
     useEffect(()=>{
         axios.get('https://reqres.in/api/unknown')
         .then (res=>{
+            //describing the company
             let companyName = "HPBMedia";
             let companyDesc = "We help CEO's build their personal brand";
-
             setData({Company:companyName,Description:companyDesc})
             setColorsData(res.data.data)
+
+            console.log("Response from api: ", res);
+            console.log("colors data: ", res.data.data)
         })
         .catch(err=>{
             console.log(err);
         })
     },[])
     return (
+        //Printing out company info 
         <>
         <h1>{Data.Company}</h1>
         <p>{Data.Description}</p>
